a21ac9dc269b62724d51174496d56b65a3a53175
juanpardo123 <129297821+juanpardo123@users.noreply.github.com>
2023-07-10 09:56:53 -0400

    database working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home";
+import Products from "./pages/products";
+import HorizonPage from "./pages/HorizonPage";
+import ContactUs from "./pages/contactUs";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/HorizonPage" element={<HorizonPage />} />
+        <Route path="/contactUs" element={<ContactUs />} />
+      </Routes>
+    </BrowserRouter>
+    // <div className="App" >
+    //   <Home />
+    // </div>
   );
 }
 
---
+++ src/components/productHolders.js
@@ -0,0 +1,10 @@
+
+const productHolders = (props) => {
+    return ( 
+        <div class="main">
+        <div id="slider" class="hidden-grow"></div>
+    </div>
+     );
+}
+ 
+export default productHolders;
